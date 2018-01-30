5203db035a6ec499511480efa2f61708b29870c5
Wallid Samy <wallidsamy@gmail.com>
2018-01-30 16:46:41 +0200

    Updated: 30-1-2018 4:46PM

--- ADCS_SW/src/B_dot.c
+++ ADCS_SW/src/B_dot.c
@@ -12,6 +12,20 @@ ErrorObject_t B_dotAlgorithm(double *TimeSinceSimulaion,double *TimeStep,double 
 	ErrorObject_t Error=Null;
 
 	uint8 i=0;
-
 	double DcGain=SatelliteParametersObject->CutOffFrequency/sqrt(5)*sqrt((2 - 2 * cos(SatelliteParametersObject->CutOffFrequency * (*TimeStep) / 2))/(1 - 2 * cos(SatelliteParametersObject->CutOffFrequency * (*TimeStep) / 2) * exp(-SatelliteParametersObject->CutOffFrequency * (*TimeStep)) + exp(-2 * SatelliteParametersObject->CutOffFrequency)));
+	double MagneticFieldDiffrentitationInBody[3]={0};
+
+	for(i=0;i<3;i++){
+			MagneticFieldDiffrentitationInBody[i]=(double)(DcGain*(MagneticFieldInBodyCoordinates[i]-SatelliteParametersObject->PreviousMagneticFieldInBody[i])+SatelliteParametersObject->PreviousMagneticFieldDiffrentiationInBody[i]*exp(-SatelliteParametersObject->CutOffFrequency*(*TimeStep)));
+
+	}
+
+	for(i=0;i<3;i++){
+			SatelliteParametersObject->PreviousMagneticFieldDiffrentiationInBody[i]=MagneticFieldDiffrentitationInBody[i];
+	}
+
+	Error=SuccessPASS;
+	return Error;
+
+
 }
